2d6af958b37ac2ba56432862011464a9b36ab04a
Andrew Zelenkov <andrey.zelenkov@mlearner.com>
2013-10-22 13:37:25 +0400

    friction: added y-variation for evaporation

--- js/view/Magnifier/Atom.js
+++ js/view/Magnifier/Atom.js
@@ -42,23 +42,34 @@ define( function( require ) {
   inherit( Node, Atom );
 
   Atom.prototype.evaporate = function() {
-    var self = this, steps = 100, dx, dy, handler = function() {
+    var self = this, steps = 100, dx, dy;
+
+    this.handler = function() {
       self.x0 += dx;
+      self.y0 -= dy;
       if ( self.x0 > 10 * self.model.width ) {
-        self.model.newStepProperty.unlink( handler );
+        self.model.newStepProperty.unlink( self.handler );
+        self.setVisible( false );
       }
     };
 
     this.x1 = this.x0 + 4 * this.model.width * (Math.round( Math.random() ) - 0.5);
     dx = (this.x1 - this.x0) / steps;
-    this.handler = handler;
-    this.model.newStepProperty.link( handler );
+    this.y1 = this.y0 + Math.random() * this.getYrange();
+    dy = (this.y1 - this.y0) / 100;
+    this.model.newStepProperty.link( self.handler );
   };
 
   Atom.prototype.reset = function() {
     this.x0 = this.options.x;
     this.y0 = this.options.y;
     this.model.newStepProperty.unlink( this.handler );
+    this.setVisible( true );
+  };
+
+  Atom.prototype.getYrange = function() {
+    var model = this.model;
+    return model.distance + model.atoms.dy * model.toEvaporate.length;
   };
 
   return Atom;
