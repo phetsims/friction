7f90453e07c95e7ae8444da2a76371285ebc24ba
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:43 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/friction-config.js
+++ js/friction-config.js
@@ -33,5 +33,6 @@ require.config( {
     FRICTION: "."
   },
 
-  urlArgs: new Date().getTime()  // cache buster to make browser reload all included scripts
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
 } );
