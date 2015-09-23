828950e54d7046e0a3857df2f92a65defc149247
Chris Malley <cmalley@pixelzoom.com>
2015-09-23 13:48:09 -0600

    Imports -> modules

--- js/friction/view/magnifier/Atom.js
+++ js/friction/view/magnifier/Atom.js
@@ -8,7 +8,7 @@
 define( function( require ) {
   'use strict';
 
-  // Imports
+  // modules
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
