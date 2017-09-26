328d8d97864f4497815f9ac77a25df1c4b939684
zepumph <michael.kauzmann@colorado.edu>
2017-09-26 14:51:01 -0800

    change book text color, https://github.com/phetsims/friction/issues/41

--- js/friction/FrictionSharedConstants.js
+++ js/friction/FrictionSharedConstants.js
@@ -16,7 +16,7 @@ define( function( require ) {
     BOTTOM_BOOK_COLOR_MACRO: 'rgb( 183, 255, 181 )', // color for the macroscopic view of the bottom book
     BOTTOM_BOOK_COLOR: 'rgb( 187, 255, 187 )', // color for the book in the magnified view
     BOTTOM_BOOK_ATOMS_COLOR: 'rgb( 0, 255, 0 )', // color for the bottom book in the magnified view,
-    BOOK_TEXT_COLOR: '#595959',
+    BOOK_TEXT_COLOR: '#404040',
     ATOM_RADIUS: 7 // in screen coordinates
   };
 
