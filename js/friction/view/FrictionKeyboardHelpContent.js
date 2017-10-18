2af1d4d7d8334f553ddab0d9c934491e6aab78d9
zepumph <michael.kauzmann@colorado.edu>
2017-10-17 17:09:55 -0800

    first pass at content complete keyboard help dialog, https://github.com/phetsims/friction/issues/45

--- js/friction/view/FrictionKeyboardHelpContent.js
+++ js/friction/view/FrictionKeyboardHelpContent.js
@@ -9,23 +9,40 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var inherit = require( 'PHET_CORE/inherit' );
   var friction = require( 'FRICTION/friction' );
-  var Panel = require( 'SUN/Panel' );
   var GeneralNavigationHelpContent = require( 'SCENERY_PHET/keyboard/help/GeneralNavigationHelpContent' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var HelpContent = require( 'SCENERY_PHET/keyboard/help/HelpContent' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Panel = require( 'SUN/Panel' );
+  var RichText = require( 'SCENERY/nodes/RichText' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+
+  // TODO: export to string files
+  var moveBookHeaderString = 'Move Book';
+  var moveBookString = 'Move book:';
+  var orString = 'or';
+  var moveInSmallerSteps = 'Move in smaller steps:';
+
+  var DEFAULT_LABEL_OPTIONS = {
+    font: HelpContent.DEFAULT_LABEL_FONT,
+    maxWidth: HelpContent.DEFAULT_TEXT_MAX_WIDTH
+  };
+
 
   /**
    * Constructor.
    *
    * @constructor
    */
-  function FrictionKeyboardHelpContent( ) {
+  function FrictionKeyboardHelpContent() {
 
-    var generalNavigationHelpContent = new GeneralNavigationHelpContent( );
+    var moveBookHelpContent = new MoveBookHelpNode();
+    var generalNavigationHelpContent = new GeneralNavigationHelpContent();
 
     var content = new HBox( {
-      children: [ generalNavigationHelpContent ],
+      children: [ moveBookHelpContent, generalNavigationHelpContent ],
       align: 'top',
       spacing: 30
     } );
@@ -35,6 +52,46 @@ define( function( require ) {
       fill: 'rgb( 214, 237, 249 )'
     } );
   }
+
+  /**
+   *
+   * @param options
+   * @constructor
+   */
+  function MoveBookHelpNode( options ) {
+
+    Node.call( this );
+    options = _.extend( {
+      // icon options
+      arrowKeysScale: 0.55,
+      verticalIconSpacing: HelpContent.DEFAULT_VERTICAL_ICON_SPACING
+
+    }, options );
+
+    // Book row
+    var moveBookText = new RichText( moveBookString, DEFAULT_LABEL_OPTIONS );
+    var arrowKeys = HelpContent.arrowKeysRowIcon( { scale: options.arrowKeysScale } );
+    var orText = new RichText( orString, DEFAULT_LABEL_OPTIONS );
+    var wasdKeys = HelpContent.wasdRowIcon();
+    var moveBookRow = new HBox( {
+      children: [ moveBookText, arrowKeys, orText, wasdKeys ],
+      spacing: HelpContent.DEFAULT_LABEL_ICON_SPACING
+    } );
+
+    // Book in smaller steps row
+    var moveInSmallerStepsText = new RichText( moveInSmallerSteps, DEFAULT_LABEL_OPTIONS );
+    var shiftPlusArrowKeys = HelpContent.shiftPlusIcon( HelpContent.arrowKeysRowIcon( { scale: options.arrowKeysScale } ) );
+    var shiftPlusWASDKeys = HelpContent.shiftPlusIcon( HelpContent.wasdRowIcon() );
+    var row = HelpContent.labelWithIconList( moveInSmallerStepsText, [ shiftPlusArrowKeys, shiftPlusWASDKeys ], {
+      spacing: HelpContent.DEFAULT_LABEL_ICON_SPACING
+    } );
+
+    var content = new VBox( { children: [ moveBookRow, row ], align: 'left', spacing: options.verticalIconSpacing } );
+
+    HelpContent.call( this, moveBookHeaderString, content, options );
+  }
+
+  inherit( HelpContent, MoveBookHelpNode );
 
   friction.register( 'FrictionKeyboardHelpContent', FrictionKeyboardHelpContent );
 
