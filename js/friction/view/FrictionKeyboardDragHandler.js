1ffc0e0780786e4feddc961e949e8c172100448c
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-12-06 10:26:49 -0700

    use KeyboardDragListener instead of KeyboardDraghandler, see phetsims/scenery#716

--- js/friction/view/FrictionKeyboardDragHandler.js
+++ js/friction/view/FrictionKeyboardDragHandler.js
@@ -13,7 +13,7 @@ define( function( require ) {
   var friction = require( 'FRICTION/friction' );
   var FrictionModel = require( 'FRICTION/friction/model/FrictionModel' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var KeyboardDragHandler = require( 'SCENERY_PHET/accessibility/KeyboardDragHandler' );
+  var KeyboardDragListener = require( 'SCENERY/accessibility/listeners/KeyboardDragListener' );
 
   /**
    * @constructor
@@ -23,13 +23,14 @@ define( function( require ) {
 
     var oldValue; // determines our delta for how the positionProperty changed every drag
 
-    KeyboardDragHandler.call( this, model.positionProperty, {
+    KeyboardDragListener.call( this, {
       positionDelta: 10,
-      shiftKeyMultiplier: .5,
-      startDrag: function() {
+      shiftPositionDelta: 5,
+      locationProperty: model.positionProperty,
+      start: function() {
         oldValue = model.positionProperty.get().copy();
       },
-      onDrag: function() {
+      drag: function() {
         var newValue = model.positionProperty.get();
         model.move( { x: newValue.x - oldValue.x, y: newValue.y - oldValue.y } );
 
@@ -46,5 +47,5 @@ define( function( require ) {
 
   friction.register( 'FrictionKeyboardDragHandler', FrictionKeyboardDragHandler );
 
-  return inherit( KeyboardDragHandler, FrictionKeyboardDragHandler );
+  return inherit( KeyboardDragListener, FrictionKeyboardDragHandler );
 } );
