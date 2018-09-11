dc8501829901049bcd10d2863bd464de28175140
zepumph <michael.kauzmann@colorado.edu>
2018-09-11 13:05:53 -0800

    add describer callbacks to FrictionDragHandler, https://github.com/phetsims/friction/issues/107

--- js/friction/view/FrictionDragHandler.js
+++ js/friction/view/FrictionDragHandler.js
@@ -12,6 +12,8 @@ define( function( require ) {
   const friction = require( 'FRICTION/friction' );
   const inherit = require( 'PHET_CORE/inherit' );
   const SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
+  const TemperatureIncreasingDescriber = require( 'FRICTION/friction/view/describers/TemperatureIncreasingDescriber' );
+  const TemperatureDecreasingDescriber = require( 'FRICTION/friction/view/describers/TemperatureDecreasingDescriber' );
   const Vector2 = require( 'DOT/Vector2' );
 
   /**
@@ -21,11 +23,20 @@ define( function( require ) {
    */
   function FrictionDragHandler( model, tandem ) {
     SimpleDragHandler.call( this, {
+      start: function() {
+
+        // a11y
+        TemperatureIncreasingDescriber.getDescriber().dragStarted();
+        TemperatureDecreasingDescriber.getDescriber().dragStarted();
+      },
       translate: function( e ) {
         model.move( new Vector2( e.delta.x, e.delta.y ) );
       },
       end: function() {
         model.bottomOffsetProperty.set( 0 );
+
+        // a11y
+        TemperatureIncreasingDescriber.getDescriber().dragEnded();
       },
       tandem: tandem
     } );
