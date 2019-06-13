c44393fc612f8e8cdbfd0c6646e6db39857e751a
zepumph <michael.kauzmann@colorado.edu>
2019-06-13 13:42:58 -0800

    update baselines and remove redundant GQ overrides, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/friction-phet-io-elements-baseline.js
+++ js/phet-io/friction-phet-io-elements-baseline.js
@@ -961,7 +961,7 @@ window.phet.phetio.phetioElementsBaseline =
     "friction.navigationBar.phetButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
