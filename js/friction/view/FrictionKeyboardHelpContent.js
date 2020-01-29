9c97581278b32a0730617fac8b37b62bbdb96909
phet-dev <phetadmin@gmail.com>
2020-01-29 03:17:19 -0700

    update copyright dates from daily grunt work

--- js/friction/view/FrictionKeyboardHelpContent.js
+++ js/friction/view/FrictionKeyboardHelpContent.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Content for the "Hot Keys and Help" dialog that can be brought up from the sim navigation bar.
