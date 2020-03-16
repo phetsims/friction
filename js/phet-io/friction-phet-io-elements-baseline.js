d4b238b80080ccd9dcfb71fc05b205333593f556
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 15:14:02 -0800

    update phetioEngine documentation, https://github.com/phetsims/studio/issues/99

--- js/phet-io/friction-phet-io-elements-baseline.js
+++ js/phet-io/friction-phet-io-elements-baseline.js
@@ -2641,7 +2641,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "UtteranceQueueIO"
     },
     "phetioEngine": {
-      "phetioDocumentation": "Central point for PhET-iO interoperability",
+      "phetioDocumentation": "Central point for PhET-iO interoperability. Please see the IO Type Documentation for API details.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
