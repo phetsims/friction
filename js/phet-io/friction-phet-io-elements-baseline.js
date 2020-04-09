04e578c8fdff416019220c2974086f49dd3ede64
zepumph <michael.kauzmann@colorado.edu>
2020-04-09 12:16:12 -0800

    rename mouseMovedAction -> mouseMoveAction, https://github.com/phetsims/scenery/issues/1043

--- js/phet-io/friction-phet-io-elements-baseline.js
+++ js/phet-io/friction-phet-io-elements-baseline.js
@@ -846,7 +846,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "ActionIO<Vector2IO, EventIO>"
     },
-    "friction.general.controller.input.mouseMovedAction": {
+    "friction.general.controller.input.mouseMoveAction": {
       "phetioDocumentation": "Emits when the mouse is moved The arguments are:<br/><ol><li>point: Vector2IO</li><br/><li>event: EventIO</li></ol>",
       "phetioDynamicElement": false,
       "phetioEventType": "USER",
