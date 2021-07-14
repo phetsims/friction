0e587b734125e09000cbaed5196734af0f45f600
phet-dev <phetadmin@gmail.com>
2021-07-14 03:40:32 -0600

    update copyright dates from daily grunt work

--- js/friction/view/describers/BreakAwayDescriber.js
+++ js/friction/view/describers/BreakAwayDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2021, University of Colorado Boulder
 
 /**
  * Describer responsible for handling the appropriate alert when atoms evaporate, or "break away" from the top book.
