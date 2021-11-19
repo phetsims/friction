88272783b30f4c21c255af4723b22c22de72a6fc
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:09 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/frictionStrings.ts
@@ -0,0 +1,96 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import friction from './friction.js';
+
+type StringsType = {
+  'friction': {
+    'title': string
+  },
+  'physics': string,
+  'chemistry': string,
+  'moveBookHeader': string,
+  'bookTitle': string,
+  'bookLabel': string,
+  'moveBook': string,
+  'moveInSmallerSteps': string,
+  'a11y': {
+    'chemistryBook': string,
+    'zoomedInChemistryBook': string,
+    'grabButtonHelpText': string,
+    'moveBookWith': string,
+    'moveInSmallerStepsWith': string,
+    'initialGrabbedNotTouching': string,
+    'lightlyOnPhysicsBook': string,
+    'moveDownToRubHarder': string,
+    'grabbedNotTouchingPattern': string,
+    'initialGrabbedTouching': string,
+    'rubFastOrSlow': string,
+    'grabbedPattern': string,
+    'amountOfAtoms': {
+      'sentence': string,
+      'fewer': string,
+      'farFewer': string,
+      'some': string,
+      'many': string
+    },
+    'jiggle': {
+      'jiggleALot': string,
+      'jiggleALittle': string,
+      'jiggleABit': string,
+      'jigglingLess': string,
+      'less': string,
+      'evenLess': string
+    },
+    'temperature': {
+      'superHot': string,
+      'capitalizedVeryHot': string,
+      'veryHot': string,
+      'hot': string,
+      'atWarm': string,
+      'atCool': string,
+      'pattern': string,
+      'thermometer': string,
+      'more': string,
+      'faster': string,
+      'evenFaster': string,
+      'superFast': string,
+      'evenCooler': string,
+      'cooler': string,
+      'nowCooler': string,
+      'warmer': string,
+      'evenHotter': string,
+      'nowHotter': string
+    },
+    'screenSummary': {
+      'summarySentencePattern': string,
+      'droppingAsAtomsJiggleLess': string,
+      'atomsJigglePattern': string,
+      'jiggleClausePattern': string,
+      'jiggleTemperatureScaleSentence': string,
+      'grabChemistryBookPlay': string,
+      'continueRubbing': string,
+      'startingChemistryBookPattern': string
+    },
+    'moveDownToRubHarderSentence': string,
+    'resetSimMoreObservationSentence': string,
+    'lightly': string,
+    'atomsJiggleTinyBitTempCool': string,
+    'frictionIncreasingAtomsJigglingTemperaturePattern': string,
+    'frictionIncreasingAtomsJigglingTemperatureFirstPattern': string,
+    'breakAwaySentenceFirst': string,
+    'breakAwaySentenceAgain': string,
+    'breakAwayNoneLeft': string,
+    'readMeOverview': string
+  }
+};
+
+const frictionStrings = getStringModule( 'FRICTION' ) as StringsType;
+
+friction.register( 'frictionStrings', frictionStrings );
+
+export default frictionStrings;
