78696e91f4165531e81ebc2e99c9b5d438f5f63b
zepumph <michael.kauzmann@colorado.edu>
2022-03-01 10:44:49 -0700

    add an enabledProperty, https://github.com/phetsims/scenery-phet/issues/720

--- js/friction/view/FrictionGrabDragInteraction.js
+++ js/friction/view/FrictionGrabDragInteraction.js
@@ -34,7 +34,10 @@ class FrictionGrabDragInteraction extends GrabDragInteraction {
       },
 
       // appended to in this type
-      listenersForDragState: []
+      listenersForDragState: [],
+
+      // TODO: it isn't clear that we want to keep this, it is just for testing in https://github.com/phetsims/scenery-phet/issues/720
+      phetioEnabledPropertyInstrumented: true
     }, options );
 
 
