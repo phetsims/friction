1611dbcfeeb94cb478f4efc44e372210a3ac5912
Sam Reid <reids@colorado.edu>
2022-05-07 09:36:30 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/frictionStrings.ts
+++ js/frictionStrings.ts
@@ -9,83 +9,83 @@ import friction from './friction.js';
 
 type StringsType = {
   'friction': {
-    'title': string
-  },
-  'physics': string,
-  'chemistry': string,
-  'moveBookHeader': string,
-  'bookTitle': string,
-  'bookLabel': string,
-  'moveBook': string,
-  'moveInSmallerSteps': string,
+    'title': string;
+  };
+  'physics': string;
+  'chemistry': string;
+  'moveBookHeader': string;
+  'bookTitle': string;
+  'bookLabel': string;
+  'moveBook': string;
+  'moveInSmallerSteps': string;
   'a11y': {
-    'chemistryBook': string,
-    'zoomedInChemistryBook': string,
-    'grabButtonHelpText': string,
-    'moveBookWith': string,
-    'moveInSmallerStepsWith': string,
-    'initialGrabbedNotTouching': string,
-    'lightlyOnPhysicsBook': string,
-    'moveDownToRubHarder': string,
-    'grabbedNotTouchingPattern': string,
-    'initialGrabbedTouching': string,
-    'rubFastOrSlow': string,
-    'grabbedPattern': string,
+    'chemistryBook': string;
+    'zoomedInChemistryBook': string;
+    'grabButtonHelpText': string;
+    'moveBookWith': string;
+    'moveInSmallerStepsWith': string;
+    'initialGrabbedNotTouching': string;
+    'lightlyOnPhysicsBook': string;
+    'moveDownToRubHarder': string;
+    'grabbedNotTouchingPattern': string;
+    'initialGrabbedTouching': string;
+    'rubFastOrSlow': string;
+    'grabbedPattern': string;
     'amountOfAtoms': {
-      'sentence': string,
-      'fewer': string,
-      'farFewer': string,
-      'some': string,
-      'many': string
-    },
+      'sentence': string;
+      'fewer': string;
+      'farFewer': string;
+      'some': string;
+      'many': string;
+    };
     'jiggle': {
-      'jiggleALot': string,
-      'jiggleALittle': string,
-      'jiggleABit': string,
-      'jigglingLess': string,
-      'less': string,
-      'evenLess': string
-    },
+      'jiggleALot': string;
+      'jiggleALittle': string;
+      'jiggleABit': string;
+      'jigglingLess': string;
+      'less': string;
+      'evenLess': string;
+    };
     'temperature': {
-      'superHot': string,
-      'capitalizedVeryHot': string,
-      'veryHot': string,
-      'hot': string,
-      'atWarm': string,
-      'atCool': string,
-      'thermometerPattern': string,
-      'pattern': string,
-      'more': string,
-      'faster': string,
-      'evenFaster': string,
-      'superFast': string,
-      'evenCooler': string,
-      'cooler': string,
-      'nowCooler': string,
-      'warmer': string,
-      'evenHotter': string,
-      'nowHotter': string
-    },
+      'superHot': string;
+      'capitalizedVeryHot': string;
+      'veryHot': string;
+      'hot': string;
+      'atWarm': string;
+      'atCool': string;
+      'thermometerPattern': string;
+      'pattern': string;
+      'more': string;
+      'faster': string;
+      'evenFaster': string;
+      'superFast': string;
+      'evenCooler': string;
+      'cooler': string;
+      'nowCooler': string;
+      'warmer': string;
+      'evenHotter': string;
+      'nowHotter': string;
+    };
     'screenSummary': {
-      'summarySentencePattern': string,
-      'droppingAsAtomsJiggleLess': string,
-      'atomsJigglePattern': string,
-      'jiggleClausePattern': string,
-      'jiggleTemperatureScaleSentence': string,
-      'grabChemistryBookPlay': string,
-      'continueRubbing': string,
-      'startingChemistryBookPattern': string,
-      'startingChemistryBookLightlyPattern': string
-    },
-    'moveDownToRubHarderSentence': string,
-    'resetSimMoreObservationSentence': string,
-    'atomsJiggleTinyBitTempCool': string,
-    'frictionIncreasingAtomsJigglingTemperaturePattern': string,
-    'frictionIncreasingAtomsJigglingTemperatureFirstPattern': string,
-    'breakAwaySentenceFirst': string,
-    'breakAwaySentenceAgain': string,
-    'breakAwayNoneLeft': string,
-    'readMeOverview': string
+      'summarySentencePattern': string;
+      'droppingAsAtomsJiggleLess': string;
+      'atomsJigglePattern': string;
+      'jiggleClausePattern': string;
+      'jiggleTemperatureScaleSentence': string;
+      'grabChemistryBookPlay': string;
+      'continueRubbing': string;
+      'startingChemistryBookPattern': string;
+      'startingChemistryBookLightlyPattern': string;
+    };
+    'moveDownToRubHarderSentence': string;
+    'resetSimMoreObservationSentence': string;
+    'atomsJiggleTinyBitTempCool': string;
+    'frictionIncreasingAtomsJigglingTemperaturePattern': string;
+    'frictionIncreasingAtomsJigglingTemperatureFirstPattern': string;
+    'breakAwaySentenceFirst': string;
+    'breakAwaySentenceAgain': string;
+    'breakAwayNoneLeft': string;
+    'readMeOverview': string;
   }
 };
 
