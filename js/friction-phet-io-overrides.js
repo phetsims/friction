186a9079b14cef8a353987962f60443ec201fa3c
Amy Rouinfar <amy.rouinfar@colorado.edu>
2022-07-25 11:52:37 -0600

    phetioFeatured overrides for https://github.com/phetsims/friction/issues/304

--- js/friction-phet-io-overrides.js
+++ js/friction-phet-io-overrides.js
@@ -1,2 +1,58 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "friction.frictionScreen.model.atomRowsToShearOffProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.contactProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.distanceBetweenBooksProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.hintProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.numberOfAtomsShearedOffProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.successfullyInteractedWithProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.topBookPositionProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.model.vibrationAmplitudeProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.atomicView.grabDragInteraction.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "friction.frictionScreen.view.atomicView.hintArrowsNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.atomicView.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.bottomBookNode.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.bottomBookNode.titleText.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.thermometerNode.percentProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.thermometerNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.topBookNode.grabDragInteraction.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "friction.frictionScreen.view.topBookNode.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "friction.frictionScreen.view.topBookNode.titleText.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };
