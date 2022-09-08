10d8d90d81cd5e8fb24ff30022ed52a60d3e3f15
phet-dev <phetadmin@gmail.com>
2022-09-08 04:28:51 -0600

    update copyright dates from daily grunt work

--- js/friction/view/TemperatureIncreasingAlerter.js
+++ js/friction/view/TemperatureIncreasingAlerter.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * This singleton type is responsible for alerting all aria-live alerts that pertain to the model/amplitude/temperature
