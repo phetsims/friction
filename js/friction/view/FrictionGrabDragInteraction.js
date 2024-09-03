f4c2ce7eb61fd6acbfa72830464fe95af9f2dfce
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-03 10:21:22 -0600

    move GrabDragInteraction to subdir, https://github.com/phetsims/scenery-phet/issues/869
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/friction/view/FrictionGrabDragInteraction.js
+++ js/friction/view/FrictionGrabDragInteraction.js
@@ -8,7 +8,7 @@
  */
 
 import merge from '../../../../phet-core/js/merge.js';
-import GrabDragInteraction from '../../../../scenery-phet/js/accessibility/GrabDragInteraction.js';
+import GrabDragInteraction from '../../../../scenery-phet/js/accessibility/grab-drag/GrabDragInteraction.js';
 import Utterance from '../../../../utterance-queue/js/Utterance.js';
 import friction from '../../friction.js';
 
