d9a7394760f8df740f6ba6b6fb70bf6627a92fac
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:30 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for friction.
+ * ESLint configuration for friction.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
